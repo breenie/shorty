4a3a8e3fa6e8f7f58f4d2fd1c7e614a9d33288c3
Chris Breen <chris@kurl.io>
2015-09-07 12:01:30 +0100

    Added infinite scrolling to  stats page.

--- app/scripts/controllers/statistics.js
+++ app/scripts/controllers/statistics.js
@@ -5,31 +5,32 @@
   'use strict';
 
   var StatisticsController = function ($scope, $http, $location) {
-    $http.get('/api/urls.json').success(function (data) {
-      $scope.urls = data.results;
-      console.log(data);
-      $scope.totalItems = data.total;
 
-    });
+    $scope.total = 0;
+    $scope.offset = 0;
+    $scope.limit = 50;
+    $scope.items = [];
+    $scope.fetching = false;
+    $scope.disabled = false;
 
-    $scope.currentPage = 1;
+    $scope.getMore = function () {
 
-    $scope.setPage = function (pageNo) {
-      $scope.currentPage = pageNo;
+      $scope.fetching = true;
+      $scope.offset += $scope.limit;
+
+      $http.get('/api/urls.json', {params: {limit: $scope.limit, offset: $scope.offset}}).then(function (response) {
+        $scope.fetching = false;
+
+        //if (response.data.length) {
+          //console.log($scope.offset);
+          $scope.items = $scope.items.concat(response.data.results);
+          //console.log(response.data.request);
+          $scope.total = response.data.total;
+        //} else {
+        //  $scope.disabled = true;
+        //}
+      });
     };
-
-    $scope.pageChanged = function() {
-      // $log.log('Page changed to: ' + $scope.currentPage);
-    };
-
-    $scope.maxSize = 5;
-    $scope.bigTotalItems = 175;
-    $scope.bigCurrentPage = 1;
-    //var result = Url.query();
-    //
-    //$scope.urls = result.then(function(result) {
-    //    return result.results;
-    //});
 
     $scope.hostname = $location.host() + (80 === $location.port() ? '' : ':' + $location.port());
   };
@@ -38,26 +39,5 @@
 
   angular.module('shortyApp')
     .controller('StatisticsController', StatisticsController);
-
-  angular.module('shortyApp').controller('TodoController', function ($scope) {
-    $scope.filteredTodos = [];
-    $scope.currentPage = 1;
-    $scope.numPerPage = 10;
-    $scope.maxSize = 5;
-
-    $scope.makeTodos = function() {
-      $scope.todos = [];
-      for (var i=1;i<=1000;i++) {
-        $scope.todos.push({ text:'todo '+i, done:false});
-      }
-    };
-    $scope.makeTodos();
-
-    $scope.$watch('currentPage + numPerPage', function() {
-      var begin = (($scope.currentPage - 1) * $scope.numPerPage), end = begin + $scope.numPerPage;
-
-      $scope.filteredTodos = $scope.todos.slice(begin, end);
-    });
-  });
 
 })(angular);
