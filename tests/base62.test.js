a2e05a7cc7dda41c1a8a70acffffaa1d5310d9a8
Chris Breen <chris@kurl.co.uk>
2018-01-22 12:40:42 +0000

    Matched base62 character dictionary with legacy PHP library so old urls can now be served correctly.

---
+++ tests/base62.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const base62 = require('../src/base62');
+
+describe('Base62', function () {
+    const known = {
+        'Ay': 680,
+        'Cc': 782,
+        'aY': 2266,
+        'cC': 2368
+    };
+
+    describe('encode', function () {
+        it('should encode known values predictably', function () {
+            Object.keys(known).forEach(element => {
+                assert.equal(base62.encode(known[element]), element);
+            });
+        });
+    });
+
+    describe('decode', function () {
+        it('should decode known values predictably', function () {
+            Object.keys(known).forEach(element => {
+                assert.equal(base62.decode(element), known[element]);
+            });
+        });
+    });
+});
