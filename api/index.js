f280466972996a560cb6425494097f860dac3926
Chris Breen <chris@kurl.co.uk>
2018-12-14 15:13:32 +0000

    🔥 Removed old routes.

--- api/index.js
+++ api/index.js
@@ -13,10 +13,7 @@ const service    = new UrlService(connection);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.static(__dirname + '/../build'));
-// app.use('/', require('./routes/html'));
 app.use('/', require('./src/routes/redirects')(service));
-// app.use('/api/urls', require('./routes/urls')(service));
 app.use('/api/urls', require('./src/routes/urls')(service));
 
 app.listen();
